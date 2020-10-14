3d401e2d205c3e8cb32109c4e3a068e44a24685b
raman kumar <raman.kumar@techblue.co.uk>
2020-10-15 00:01:36 +0530

    PRCV-7042

--- src/app/shared/authguard.ts
+++ src/app/shared/authguard.ts
@@ -11,21 +11,23 @@ export class AuthGuard implements CanActivate {
     ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const currentUser = this.commonService.checkAuthToken();
-        const userDetails = this.commonService.getItem(PROPCO.LOGIN_DETAILS, true);
-        if (currentUser) {
-            // check if route is restricted by role
-            if (route.data.roles && route.data.roles.indexOf(userDetails.role) === -1) {
-                // role not authorised so redirect to home page
-                this.router.navigate(['/page-not-found'], { replaceUrl: true });
-                return false;
-            }
-            // authorised so return true
+        const accessToken = this.commonService.getItem(PROPCO.ACCESS_TOKEN);
+        const webKey = this.commonService.getItem(PROPCO.WEB_KEY);
+
+        if(accessToken && webKey){
             return true;
         }
-        // not logged in so redirect to login page with the return url
-        this.commonService.setItem(PROPCO.PREVIOUS_URL, state.url);
-        this.router.navigate(['/login'], { replaceUrl: true });
-        return false;
+        const ssoKey = encodeURIComponent(route.queryParams.ssoKey);
+        return new Promise<boolean>((resolve, reject) => {
+            this.commonService.authenticateSsoToken(ssoKey).toPromise().then(response => {
+                this.commonService.setItem(PROPCO.ACCESS_TOKEN, response.loginId);
+                this.commonService.setItem(PROPCO.WEB_KEY, response.webKey);
+                resolve(true);
+            }, err => {
+                // resolve(true);
+                reject(false);
+            });
+
+        });
     }
 }
