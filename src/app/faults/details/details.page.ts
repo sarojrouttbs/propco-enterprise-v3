d42d1183fa44780b720899b8baa3afd37ddf310c
saroj rout <saroj.rout@techblue.co.uk>
2020-10-20 11:25:44 +0530

    added fault services and form groups

--- src/app/faults/details/details.page.ts
+++ src/app/faults/details/details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { forkJoin } from 'rxjs';
+import { FaultsService } from '../faults.service';
 import { catList, propertyData } from './cat.json';
 
 @Component({
@@ -10,8 +13,18 @@ export class DetailsPage implements OnInit {
 
   catList: any[] = catList;
   propertyData = propertyData;
-  pageNo = 3;
+  pageNo = 1;
   faultDetails: any = {};
+  propertyId = '5eae3eee-f99b-11e8-bd34-0cc47a54d954';
+  propertyDetails;
+  propertyTenancyDetails;
+  propertyHMODetails;
+  addtionalInfo;
+  describeFaultForm: FormGroup;
+  faultDetailsForm: FormGroup;
+  addAdditionalDetForm: FormGroup;
+  reportedByFOrm: FormGroup;
+  accessInfoForm: FormGroup;
 
   categoryIconList = [
     'assets/images/fault-categories/alarms-and-smoke-detectors.svg',
@@ -29,17 +42,154 @@ export class DetailsPage implements OnInit {
     'assets/images/fault-categories/water-and-leaks.svg'
   ];
 
-  constructor() { 
+  constructor(private faultService: FaultsService, private fb: FormBuilder) {
+  }
+
+  ionViewDidEnter() {
+    this.initiateFault();
   }
 
   ngOnInit() {
-    this.faultDetails.urgencyStatus = 1;
     this.catList.map((cat, index) => {
       cat.imgPath = this.categoryIconList[index];
     });
   }
 
-  goToPriorityPage(pageNo){
+  goToPriorityPage(pageNo) {
     this.pageNo = pageNo;
   }
+
+  initiateFault() {
+    this.initiateForms();
+    this.initialApiCall();
+  }
+
+  initiateForms() {
+    this.initDescribeFaultForm();
+    this.initFaultDetailsForm();
+    this.initaddAdditionalDetForm();
+    this.initReportedByForm();
+    this.initAccessInfiForm();
+  }
+
+  initDescribeFaultForm(): void {
+    this.describeFaultForm = this.fb.group({
+      title: ['', Validators.required],
+      urgencyStatus: ['', Validators.required]
+    });
+  }
+
+  initFaultDetailsForm(): void {
+    this.faultDetailsForm = this.fb.group({
+      notes: ['', Validators.required],
+      additionalInfo: this.fb.array([])
+    });
+  }
+
+  initaddAdditionalDetForm(): void {
+    this.describeFaultForm = this.fb.group({
+      label: ['', Validators.required],
+      value: ['', Validators.required]
+    });
+  }
+
+  initAccessInfiForm(): void {
+    this.describeFaultForm = this.fb.group({
+      tenantNotes: '',
+      areOccupiersVulnerable: '',
+      isTenantPresenceRequired: ['', Validators.required]
+    });
+  }
+
+  initReportedByForm(): void {
+    this.describeFaultForm = this.fb.group({
+      reportedBy: ['', Validators.required],
+      agreementId: ['', Validators.required],
+      reportedById: '',
+      propertyId: '',
+      isDraft: false,
+      tenantId: ['', Validators.required],
+      title: [{ value: '', disabled: true }],
+      forename: [{ value: '', disabled: true }],
+      surname: [{ value: '', disabled: true }],
+      email: [{ value: '', disabled: true }],
+      mobile: [{ value: '', disabled: true }],
+      homeTelephoneNo: [{ value: '', disabled: true }]
+    });
+  }
+
+  initialApiCall() {
+    forkJoin([
+      this.getFaultAdditionalInfo(),
+      this.getPropertyById(),
+      this.getPpropertyTenancies(),
+      this.getHMOLicenceDetails()
+    ]);
+  }
+
+  getPropertyById(): void {
+    this.faultService.getPropertyById(this.propertyId).subscribe(
+      res => {
+        if (res && res.data) {
+          this.propertyDetails = res.data;
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  getPpropertyTenancies(): void {
+    this.faultService.getPpropertyTenancies(this.propertyId).subscribe(
+      res => {
+        if (res && res.data) {
+          this.propertyTenancyDetails = res.data;
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  getHMOLicenceDetails(): void {
+    this.faultService.getHMOLicenceDetailsAgainstProperty(this.propertyId).subscribe(
+      res => {
+        if (res && res.data) {
+          this.propertyHMODetails = res.data;
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  getFaultAdditionalInfo(): void {
+    this.faultService.getFaultAdditionalInfo().subscribe(
+      res => {
+        if (res) {
+          this.addtionalInfo = res.data;
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  getAgreementDetails(agreementId): void {
+    this.faultService.getPpropertyAgreementDetails(this.propertyId, agreementId).subscribe(
+      res => {
+        if (res && res.data) {
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+
 }
--- src/app/faults/faults.service.ts
+++ src/app/faults/faults.service.ts
@@ -16,4 +16,31 @@ export class FaultsService {
     return this.httpClient.get(environment.API_BASE_URL + `faults/${faultId}/notes`);
   }
 
+  getPropertyById(propertyId: string): Observable<any> {
+    return this.httpClient.get(environment.API_BASE_URL + `properties/${propertyId}`);
+  }
+
+  getPpropertyTenancies(propertyId: string): Observable<any> {
+    let params = new HttpParams();
+    params.set('status', '1');
+    params.set('status', '2');
+    params.set('status', '5');
+    params.set('status', '6');
+    return this.httpClient.get(environment.API_BASE_URL + `properties/${propertyId}/tenancies`, { params: params });
+  }
+
+  getPpropertyAgreementDetails(propertyId: string, agreementId: string): Observable<any> {
+    let params = new HttpParams();
+    params.set('agreementId', agreementId);
+    return this.httpClient.get(environment.API_BASE_URL + `properties/${propertyId}/tenants`, { params: params });
+  }
+
+  getHMOLicenceDetailsAgainstProperty(propertyId: string): Observable<any> {
+    return this.httpClient.get(environment.API_BASE_URL + `properties/${propertyId}/hmo`);
+  }
+
+  getFaultAdditionalInfo(): Observable<any> {
+    return this.httpClient.get(environment.API_BASE_URL + `faults/additional-info`);
+  }
+
 }
