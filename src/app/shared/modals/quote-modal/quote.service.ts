8e4e56ffa77d7f7e85df517c8cd19ed206cfbc00
saroj rout <saroj.rout@techblue.co.uk>
2020-12-15 23:08:20 +0530

    quote upload functionality with service integration

--- src/app/shared/modals/quote-modal/quote.service.ts
+++ src/app/shared/modals/quote-modal/quote.service.ts
@@ -1,7 +1,30 @@
+import { environment } from './../../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+environment
 @Injectable()
 export class QuoteService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  uploadFaultDocument(formData: FormData, faultId): Observable<any> {
+    return this.httpClient.post(environment.API_BASE_URL + `faults/${faultId}/documents/upload`, formData);
+  }
+
+  uploadMaintDocument(formData: FormData, maintenanceId): Observable<any> {
+    return this.httpClient.post(environment.API_BASE_URL + `maintenance/${maintenanceId}/documents/upload`, formData);
+  }
+
+  saveNotificationQuoteAmount(body: any, faultNotificationId): Observable<any> {
+    return this.httpClient.post(environment.API_BASE_URL + `faults/notifications/${faultNotificationId}/response/quote-amount`, body);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(`${operation} failed: ${error.message}`);
+      return throwError(error);
+    };
+  }
 }
