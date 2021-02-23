605c085b97fd5ab3afd17938aa05f243e532f8ec
raman kumar <raman.kumar@techblue.co.uk>
2021-02-23 09:14:25 +0530

    Escalation filter

--- src/app/faults/dashboard/dashboard.page.ts
+++ src/app/faults/dashboard/dashboard.page.ts
@@ -535,7 +535,7 @@ export class DashboardPage implements OnInit {
     }
 
     if (this.filterForm.get('escalation').value) {
-      this.fs.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21);
+      this.fs.push(18);
     }
 
     const filteredStatus: any = [];
