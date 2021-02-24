b154af14c56511ba8762b820e3731c5e6c6e7ab8
saroj rout <saroj.rout@techblue.co.uk>
2021-02-24 09:54:29 +0000

    cancelled & closed faults check added for all the stages

--- src/app/faults/details/details.page.ts
+++ src/app/faults/details/details.page.ts
@@ -2035,10 +2035,11 @@ export class DetailsPage implements OnInit {
   getPendingHours() {
     let hours = 0;
     const currentDateTime = this.commonService.getFormatedDateTime(new Date());
-    if (this.cliNotification.nextChaseDueAt) {
-      const diffInMs = Date.parse(this.cliNotification.nextChaseDueAt) - Date.parse(currentDateTime);
-      hours = diffInMs / 1000 / 60 / 60;
+    if (this.cliNotification && this.faultDetails.status !== 18 && this.cliNotification.nextChaseDueAt) {
+      let msec = new Date(this.cliNotification.nextChaseDueAt).getTime() - new Date(currentDateTime).getTime();
+      let mins = Math.floor(msec / 60000);
+      let hrs = Math.floor(mins / 60);
+      this.cliNotification.hoursLeft = hrs != 0 ? `${hrs} hours` : `${mins} minutes`;
     }
-    return hours > 0 ? Math.floor(hours) : 0;
   }
 }
