2fdf50fb4cab25a7dfb34fc10573e70850fe21af
gaurid-iprogrammer <gaurid@iprogrammer.com>
2021-03-03 14:22:08 +0530

    FF-1290

--- src/app/shared/modals/contractor-details-modal/contractor-details-modal.page.ts
+++ src/app/shared/modals/contractor-details-modal/contractor-details-modal.page.ts
@@ -33,8 +33,8 @@ export class ContractorDetailsModalPage implements OnInit {
       company: ['', Validators.required],
       name: ['', Validators.required],
       telephone: ['', Validators.required],
-      email: ['', [Validators.required, ValidationService.emailValidator]],
-      estimatedVisitAt: ['', Validators.required],
+      email: ['', [ValidationService.emailValidator]],
+      estimatedVisitAt: [''],
       notes: ''
     });
   }
@@ -48,7 +48,7 @@ export class ContractorDetailsModalPage implements OnInit {
       estimatedVisitAt: this.llContractorDetails.estimatedVisitAt,
       notes: this.llContractorDetails.notes
     });
-    this.contractorDetailForm.disable();
+    // this.contractorDetailForm.disable();
   }
 
   save() {
