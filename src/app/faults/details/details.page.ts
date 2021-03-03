bb38c9e2313c93d747320a420e88803ba7acd2cd
puneetkumar <puneet.kumar@techblue.col.uk>
2021-03-03 11:31:59 +0530

    cosmetic changes

--- src/app/faults/details/details.page.ts
+++ src/app/faults/details/details.page.ts
@@ -317,7 +317,7 @@ export class DetailsPage implements OnInit {
       this.faultDetails = <FaultModels.IFaultResponse>{};
       this.faultDetails.status = 1;
     }
-    // this.commonService.showLoader();
+    this.commonService.showLoader();
     forkJoin([
       this.getFaultAdditionalInfo(),
       this.getPropertyById(),
