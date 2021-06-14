e786871cd2312c897ff801bf7d2432892b078280
gaurid-iprogrammer <gaurid@iprogrammer.com>
2021-06-14 16:04:23 +0530

    FF-2098

--- src/app/shared/modals/escalate-modal/escalate-modal.page.ts
+++ src/app/shared/modals/escalate-modal/escalate-modal.page.ts
@@ -17,6 +17,8 @@ export class EscalateModalPage implements OnInit {
   escalateForm: FormGroup;
   userDetails: any;
   faultId:string;
+  unSavedData = false;
+  showLoader = false;
 
   constructor(
     private navParams: NavParams,
@@ -38,19 +40,35 @@ export class EscalateModalPage implements OnInit {
   }
 
   escalateFault() {
+    this.showLoader = true;
     if (this.escalateForm.valid) {
       const requestObj = this.escalateForm.value;
       this.faultsService.escalateFault(this.faultId, requestObj).subscribe(res => {
+        this.showLoader = false;
         this.modalController.dismiss('success');
       }, err => {
+        this.showLoader = false;
         this.commonService.showMessage(err.message, 'Add Note', 'error');
       });
     } else {
+      this.showLoader = false;
       // this.commonService.showMessage('Please fill all the required fields.', 'Add Note', 'error');
       this.escalateForm.markAllAsTouched();
     }
   }
 
+  async onCancel() {
+    if(this.escalateForm.value.escalationReason || this.escalateForm.value.escalationReason){
+      this.unSavedData = true;
+    }else{
+      this.dismiss();
+    }
+  }
+
+  continue(){
+    this.unSavedData = false;
+  }
+  
   dismiss() {
     this.modalController.dismiss();
   }
