a64c1f42c9209b5858a9c3568ad15ac7cebcbd67
puneetkumar <puneet.kumar@techblue.col.uk>
2021-11-24 17:32:33 +0530

    PW-4996

---
+++ src/app/tob/tob.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tob',
+  templateUrl: './tob.page.html',
+  styleUrls: ['./tob.page.scss'],
+})
+export class TobPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
