4c3d158ae5ae3cc56c6ea0b955cfdb124c9386b0
Raman Kumar <raman.kumar@techblue.co.uk>
2021-12-14 15:01:42 +0530

    Prod fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_BASE_URL: "https://saas-qa.propco.co.uk/v3-dev/api/",
-  MEDIA_HOST_URL: 'https://saas-qa.propco.co.uk/demolocal/',
+  MEDIA_HOST_URL: 'https://saas-qa.propco.co.uk/demolocal/'
 };
