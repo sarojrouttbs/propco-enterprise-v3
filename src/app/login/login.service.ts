b1b74c44794bcfc4fa3cd201f9ca6dfc6fe5b9ae
saroj.rout <saroj.rout@techblue.co.uk>
2022-04-20 10:21:25 +0530

    PRCV-7429  PRCV-7227 PRCV-7533

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getDomains(): Observable<any> {
+    return this.httpClient.get(environment.API_BASE_URL + `agents/domains`);
+  }
+
+  authenticateUser(requestObj): Observable<any> {
+    return this.httpClient.post(environment.API_BASE_URL + `agents/authenticate`, requestObj);
+  }
 }
