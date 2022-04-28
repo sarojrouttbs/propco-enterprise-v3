944fe2e1aecc18f8bf1ef0e75f986c111713a908
https://gitlab.techblue.co.uk/subodh.gawade <subodh.gawade@techblue.co.uk>
2022-04-28 16:46:09 +0530

    PRCV - 7573 : Map Enhancement

--- src/app/agent/workspace/property/google-map/google-map.component.ts
+++ src/app/agent/workspace/property/google-map/google-map.component.ts
@@ -1,56 +1,115 @@
-import {
-  Component,
-  OnInit,
-  Input
-} from "@angular/core";
+import { Component, OnInit, Input, NgZone } from "@angular/core";
+import { Observable } from "rxjs";
+import { MapsAPILoader } from "@agm/core";
+import { tap, map, switchMap } from "rxjs/operators";
+import { from, of } from "rxjs";
 
+declare var google: any;
 
 @Component({
-  selector: 'app-google-map',
-  templateUrl: './google-map.component.html',
-  styleUrls: ['./google-map.component.scss'],
+  selector: "app-google-map",
+  templateUrl: "./google-map.component.html",
+  styleUrls: ["./google-map.component.scss"],
 })
 export class GoogleMapComponent implements OnInit {
+  private geocoder: any;
 
-lat:Number;
-lng:Number;
-centerLongitude;
-centerLatitude;
-@Input() propertyData;
-propertyAddress:any;
+  lat: Number;
+  lng: Number;
+  centerLongitude: any;
+  centerLatitude: any;
+  @Input() propertyData: { address: any };
+  propertyAddress: any;
 
+  markers: marker[] = [];
 
-markers: marker[] = [
-];
+  constructor(private _zone: NgZone, private mapLoader: MapsAPILoader) {}
 
-ngOnInit(): void {    
-  if(this.propertyData?.address){
+  ngOnInit(): void {
     this.propertyAddress = this.propertyData?.address;
-    this.setCurrentPosition();
+
+    if (
+      Number(this.propertyAddress.latitude) &&
+      Number(this.propertyAddress.longitude)
+    ) {
+      this.setCurrentPosition(
+        Number(this.propertyAddress.latitude),
+        Number(this.propertyAddress.longitude)
+      );
+    } else {
+      const addressStr =
+        this.propertyAddress?.addressLine1 +
+        " " +
+        this.propertyAddress?.addressLine1 +
+        " " +
+        this.propertyAddress?.addressLine3 +
+        " " +
+        this.propertyAddress?.town +
+        " " +
+        this.propertyAddress?.county +
+        " " +
+        this.propertyAddress?.postcode;
+      this.addressToCoordinates(addressStr);
+    }
   }
-}
 
+  private initGeocoder() {
+    this.geocoder = new google.maps.Geocoder();
+  }
 
-  private setCurrentPosition() {
-    this.lat = Number(this.propertyAddress.latitude);
-    this.lng = Number(this.propertyAddress.longitude);
-      this.markers.push({
-        lat: Number(this.propertyAddress.latitude) ,
-        lng: Number(this.propertyAddress.longitude),
-        label: "A",
-        draggable: true,
-        content: "InfoWindow content"
+  waitForMapsToLoad(): Observable<boolean> {
+    if (!this.geocoder) {
+      return from(this.mapLoader.load()).pipe(
+        tap(() => this.initGeocoder()),
+        map(() => true)
+      );
+    }
+    return of(true);
+  }
+
+  geocodeAddress(location: string): Observable<any> {
+    return this.waitForMapsToLoad().pipe(
+      switchMap(() => {
+        return new Observable((observer) => {
+          this.geocoder.geocode({ address: location }, (results, status) => {
+            if (status == google.maps.GeocoderStatus.OK) {
+              observer.next({
+                lat: results[0].geometry.location.lat(),
+                lng: results[0].geometry.location.lng(),
+              });
+            } else {
+              observer.next({ lat: 0, lng: 0 });
+            }
+            observer.complete();
+          });
+        });
       })
-    
-   
+    );
+  }
+
+  addressToCoordinates(address) {
+    this.geocodeAddress(address).subscribe((location: Location) => {
+      this.lat = location["lat"];
+      this.lng = location["lng"];
+      this.setCurrentPosition(this.lat, this.lng);
+    });
+  }
+
+  private setCurrentPosition(lat, lng) {
+    this.lat = lat;
+    this.lng = lng;
+    this.markers.push({
+      lat: lat,
+      lng: lng,
+      draggable: false,
+      
+    });
   }
 }
 
 // just an interface for type safety.
 interface marker {
-lat: number;
-lng: number;
-label?: string;
-draggable: boolean;
-content: string;
+  lat: number;
+  lng: number;
+  draggable: boolean
 }
