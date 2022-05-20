62af409e06bd0fbacf1df2e4dcd01c0f0e772739
sagart-iprogrammer <sagart@iprogrammer.com>
2022-05-20 12:35:09 +0530

    PRCV-7610

--- src/app/tob/application-list/application-list.module.ts
+++ src/app/tob/application-list/application-list.module.ts
@@ -9,6 +9,9 @@ import { ApplicationListPageRoutingModule } from './application-list-routing.mod
 import { ApplicationListPage } from './application-list.page';
 import { MaterialModule } from 'src/app/material.module';
 import { PipesModule } from 'src/app/shared/pipes/pipes.module';
+import { HoldingDepositePaidModalPageModule } from 'src/app/shared/modals/holding-deposite-paid-modal/holding-deposite-paid-modal.module';
+import { NegotiateModalPageModule } from 'src/app/shared/modals/negotiate-modal/negotiate-modal.module';
+import { TermsAndConditionModalPageModule } from 'src/app/shared/modals/terms-and-condition-modal/terms-and-condition-modal.module';
 
 @NgModule({
   imports: [
@@ -18,7 +21,10 @@ import { PipesModule } from 'src/app/shared/pipes/pipes.module';
     ApplicationListPageRoutingModule,
     MaterialModule,
     ReactiveFormsModule,
-    PipesModule
+    PipesModule,
+    HoldingDepositePaidModalPageModule,
+    NegotiateModalPageModule,
+    TermsAndConditionModalPageModule
   ],
   declarations: [ApplicationListPage]
 })
