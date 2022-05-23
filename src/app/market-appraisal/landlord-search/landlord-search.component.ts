0b2f2b81c85dbb440741532554637e8179312471
saroj.rout <saroj.rout@techblue.co.uk>
2022-05-23 18:06:41 +0530

    PRCV-7735, PRCV-7624 PRCV-7667

--- src/app/market-appraisal/landlord-search/landlord-search.component.ts
+++ src/app/market-appraisal/landlord-search/landlord-search.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input, OnInit,Output,EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { HttpParams } from '@angular/common/http';
 import { SolrService } from 'src/app/solr/solr.service';
 import { AgentService } from 'src/app/agent/agent.service';
 import { MarketAppraisalService } from 'src/app/market-appraisal/market-appraisal.service';
-import { market_appraisal ,search_Text} from 'src/app/shared/constants';
+import { market_appraisal, search_Text } from 'src/app/shared/constants';
+import { CommonService } from 'src/app/shared/services/common.service';
 
 @Component({
   selector: 'app-landlord-search',
   templateUrl: './landlord-search.component.html',
   styleUrls: ['./landlord-search.component.scss'],
-  providers:[AgentService]
+  providers: [AgentService]
 })
 export class LandlordSearchComponent implements OnInit {
 
@@ -27,18 +28,21 @@ export class LandlordSearchComponent implements OnInit {
   updateLandlordDppDetails;
   propertyLandlords;
   propertyData;
-  isPropertyItemAvailable=false;
+  isPropertyItemAvailable = false;
   entityControl = new FormControl(['Property']);
-  propertySuggestion ;
+  propertySuggestion;
 
-  constructor(private marketAppraisalService:MarketAppraisalService,private solrService:SolrService, private agentService:AgentService) { }
+  constructor(private marketAppraisalService: MarketAppraisalService,
+    private solrService: SolrService,
+    private agentService: AgentService,
+    private commonService: CommonService) { }
 
   ngOnInit() {
 
   }
 
-   SelectLandlord(item){
-     this.searchTerm = '';
+  selectLandlord(item) {
+    this.searchTerm = '';
     this.getLandlordDetails(item.entityId);
     this.getLandlordProperties(item.entityId);
     this.isItemAvailable = false;
@@ -46,139 +50,142 @@ export class LandlordSearchComponent implements OnInit {
 
   }
 
-  SelectProperty(item){
+  SelectProperty(item) {
     this.marketAppraisalService.propertyChangeEvent(item);
     this.searchTerm = '';
     this.isPropertyItemAvailable = false;
     this.initializePropertyItems();
   }
 
-  onFocus(){
-    if(this.type === market_appraisal.contact_type && this.suggestions.length ){
-        this.isItemAvailable = true;
+  onFocus() {
+    if (this.type === market_appraisal.contact_type && this.suggestions.length) {
+      this.isItemAvailable = true;
     }
-    if(this.type ===  market_appraisal.property_type && this.propertySuggestion.length ){
+    if (this.type === market_appraisal.property_type && this.propertySuggestion.length) {
       this.isPropertyItemAvailable = true;
-     }
-  }
-
-  reset(){
-    this.searchTerm = '';
-    if(this.type === market_appraisal.contact_type){
-      this.marketAppraisalService.landlordValueChange('reset');
-    }
-    if(this.type === market_appraisal.property_type){
-      this.marketAppraisalService.propertyChangeEvent('reset');
     }
   }
 
- getItems(ev: any) {
+  async reset() {
+    const confirm = await this.commonService.showConfirm('Market Appraisal', 'Are you sure you want to clear the form?.', '', 'Yes', 'No');
+    if (confirm) {
+      this.searchTerm = '';
+      if (this.type === market_appraisal.contact_type) {
+        this.marketAppraisalService.landlordValueChange('reset');
+      }
+      if (this.type === market_appraisal.property_type) {
+        this.marketAppraisalService.propertyChangeEvent('reset');
+      }
+    }
+  }
 
-  if(this.type === market_appraisal.contact_type){
-    this.initializeItems();
-    const searchText = ev.target.value;
-    if (searchText && searchText.trim() !== '' && searchText.length > 3) {
-      this.getSuggestions(this.prepareSearchParams(searchText,search_Text.lanlord));
-    } else {
+  getItems(ev: any) {
+
+    if (this.type === market_appraisal.contact_type) {
+      this.initializeItems();
+      const searchText = ev.target.value;
+      if (searchText && searchText.trim() !== '' && searchText.length > 3) {
+        this.getSuggestions(this.prepareSearchParams(searchText, search_Text.lanlord));
+      } else {
+        this.isItemAvailable = false;
+      }
+    }
+
+    if (this.type === market_appraisal.property_type) {
+      this.initializePropertyItems();
+      const searchText = ev.target.value;
+      if (searchText && searchText.trim() !== '' && searchText.length > 3) {
+        this.getSuggestions(this.prepareSearchParams(searchText, search_Text.property));
+      } else {
+        this.isPropertyItemAvailable = false;
+      }
+    }
+  }
+
+
+  serchItem() {
+    const searchText = this.searchTermControl.value;
+    if (this.type === market_appraisal.contact_type && searchText && searchText.trim() !== '' && searchText.length > 3) {
+      this.getSuggestions(this.prepareSearchParams(searchText, search_Text.lanlord));
+    }
+
+    if (this.type === market_appraisal.property_type && searchText && searchText.trim() !== '' && searchText.length > 3) {
+      this.getSuggestions(this.prepareSearchParams(searchText, search_Text.property));
+    }
+
+  }
+
+  private getLandlordDetails(landlordId) {
+    const promise = new Promise((resolve, reject) => {
+      this.marketAppraisalService.getLandlordDetails(landlordId).subscribe(
+        res => {
+          this.landlordDetails = res ? res : [];
+          this.marketAppraisalService.landlordValueChange(this.landlordDetails);
+          resolve(this.landlordDetails);
+        },
+        error => {
+          reject(null);
+        }
+      );
+    });
+    return promise;
+  }
+
+  private getLandlordProperties(landlordId) {
+    const promise = new Promise((resolve, reject) => {
+      this.marketAppraisalService.getLandlordProperties(landlordId).subscribe(
+        res => {
+          this.propertySuggestion = res ? res.data : [];
+        },
+        error => {
+          reject(null);
+        }
+      );
+    });
+    return promise;
+  }
+
+  private getSuggestions(params: HttpParams) {
+    this.showLoader = true;
+    this.solrService.entityGetSuggestion(params).subscribe((res) => {
+
+
+      if (this.type === market_appraisal.property_type) {
+        this.propertySuggestion = res ? res : [];
+        if (this.propertySuggestion.length > 0) {
+          this.isPropertyItemAvailable = true;
+        }
+      } else {
+        this.suggestions = res ? res : [];
+        if (this.suggestions.length > 0) {
+          this.isItemAvailable = true;
+        }
+      }
+      this.showLoader = false;
+    });
+  }
+
+
+  private prepareSearchParams(searchText: string, searchTypes) {
+    return (
+      new HttpParams()
+        .set('searchTerm', searchText)
+        .set('searchTypes', searchTypes)
+        .set('searchSwitch', 'true')
+        .set('hideLoader', 'true')
+    );
+  }
+
+  hideSuggestion() {
+    setTimeout(() => {
       this.isItemAvailable = false;
-    }
+    }, 200);
   }
- 
-  if(this.type === market_appraisal.property_type){
-    this.initializePropertyItems();
-    const searchText = ev.target.value;
-    if (searchText && searchText.trim() !== '' && searchText.length > 3) {
-      this.getSuggestions(this.prepareSearchParams(searchText,search_Text.property));
-    } else {
-      this.isPropertyItemAvailable = false;
-    }
+
+  initializeItems() {
+    this.suggestions = [];
   }
-}
-
-
-serchItem(){
-  const searchText = this.searchTermControl.value;
-  if (this.type === market_appraisal.contact_type && searchText && searchText.trim() !== '' && searchText.length > 3) {
-    this.getSuggestions(this.prepareSearchParams(searchText,search_Text.lanlord));
-  } 
-
-  if (this.type === market_appraisal.property_type && searchText && searchText.trim() !== '' && searchText.length > 3) {
-    this.getSuggestions(this.prepareSearchParams(searchText,search_Text.property));
-  } 
-  
-}
-
-private getLandlordDetails(landlordId) {
-  const promise = new Promise((resolve, reject) => {
-    this.marketAppraisalService.getLandlordDetails(landlordId).subscribe(
-      res => {
-        this.landlordDetails = res ? res : [];
-        this.marketAppraisalService.landlordValueChange(this.landlordDetails);
-        resolve(this.landlordDetails);
-      },
-      error => {
-        reject(null);
-      }
-    );
-  });
-  return promise;
-}
-
-private getLandlordProperties(landlordId) {
-  const promise = new Promise((resolve, reject) => {
-    this.marketAppraisalService.getLandlordProperties(landlordId).subscribe(
-      res => {
-        this.propertySuggestion = res ? res.data : [];
-      },
-      error => {
-        reject(null);
-      }
-    );
-  });
-  return promise;
-}
-
-private getSuggestions(params: HttpParams) {
-  this.showLoader = true;
-  this.solrService.entityGetSuggestion(params).subscribe((res) => {
-
-
-if (this.type === market_appraisal.property_type){
-  this.propertySuggestion = res ? res : [];
-  if (this.propertySuggestion.length > 0) {
-    this.isPropertyItemAvailable = true;
+  initializePropertyItems() {
+    this.propertySuggestion = [];
   }
-}else{
-  this.suggestions = res ? res : [];
-  if (this.suggestions.length > 0) {
-    this.isItemAvailable = true;
-  }
-}
-    this.showLoader = false;
-  });
-}
-
-
-private prepareSearchParams(searchText: string,searchTypes) {
-  return (
-    new HttpParams()
-      .set('searchTerm', searchText)
-      .set('searchTypes',searchTypes )
-      .set('searchSwitch', 'true')
-      .set('hideLoader', 'true')
-  );
-}
-
-hideSuggestion() {
-  setTimeout(() => {
-    this.isItemAvailable = false;
-  }, 200);
-}
-
-initializeItems() {
-  this.suggestions = [];
-}
-initializePropertyItems() {
-  this.propertySuggestion = [];
-}
 }
