a024d512f2a1ac2cb780074ef4923efa0a09d824
gaurid-iprogrammer <gaurid@iprogrammer.com>
2022-06-24 12:30:19 +0530

    PRCV-7794, PRCV-7857

--- src/app/agent/agent.service.ts
+++ src/app/agent/agent.service.ts
@@ -166,9 +166,4 @@ export class AgentService {
   getChangeHistory(params: any): Observable<any> {
     return this.httpClient.get(environment.API_BASE_URL + `change-history`, { params });
   }
-
-  getPropertyNotes(propertyId: string, params): Observable<any> {
-    return this.httpClient.get(
-      environment.API_BASE_URL + `properties/${propertyId}/notes`, { params });
-  }
 }
