b75f40296f3c166b1316d445138163885628931f
sagart-iprogrammer <sagart@iprogrammer.com>
2022-07-15 13:27:58 +0530

    PRCV-7711

---
+++ src/app/agent/workspace/property/admin/tenancy/agreement-details/agreement-details.component.ts
@@ -0,0 +1,174 @@
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { DATE_FORMAT, DEFAULTS, PROPCO } from 'src/app/shared/constants';
+import { CommonService } from 'src/app/shared/services/common.service';
+import { AgentService } from 'src/app/agent/agent.service';
+
+@Component({
+  selector: 'app-agreement-details',
+  templateUrl: './agreement-details.component.html',
+  styleUrls: ['./agreement-details.component.scss'],
+})
+export class AgreementDetailsComponent implements OnInit {
+  notAvailable = DEFAULTS.NOT_AVAILABLE;
+  DATE_FORMAT = DATE_FORMAT;
+  agreementDetailsForm: FormGroup;
+  tenantNameList = [];
+  lookupdata: any;
+  frequencyTypes: any;
+  agreementStatuses: any;
+  managementTypes: any;
+  contractTypes: any;
+  agreementPeriodicities: any;
+  inventoryPreparedBy: any;
+  agreementDetails: any;
+  rentFrequencyList = Array.from(Array(100).keys());
+  agreementTenantDetails: any;
+  @Input() selectedTenant;
+
+  constructor(private agentService: AgentService, private _formBuilder: FormBuilder, private commonService: CommonService) { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.selectedTenant && changes.selectedTenant.currentValue) {
+      this.selectedTenant = this.selectedTenant;
+      this.initAgreementDetailsAPI();
+    }
+  }
+
+  ngOnInit() {
+    this.rentFrequencyList.shift();
+    this.initAPI();
+  }
+
+  private initAPI() {
+    this.getLookupData();
+    this.creatForm();
+  }
+
+  private async initAgreementDetailsAPI() {
+    this.agreementDetails = await this.getAgreementDetails();
+    this.agreementTenantDetails = this.agreementDetails.agreementTenantDetail;
+    this.patchAgreementDetails();
+  }
+
+  creatForm() {
+    this.agreementDetailsForm = this._formBuilder.group({
+      tenantName: [],
+      rent: [],
+      rentFrequency: [],
+      frequencyType: [],
+      commissionPercentage: [],
+      managementType: [], // pending from API
+      totalOccupants: [], // pending from API
+      noOfOccupiers: [],
+      status: [],
+      nextClaimDate: [],
+      dayRentDue: [],
+      directDebitDueDay: [],
+      noOfChildren: [],
+      noOfHouseholds: [],
+      tenancy: [],
+      room: [],
+      lastRentRequestDate: [],
+      occupier: [],
+      permittedDelay: [],
+      noOfPermittedOccupier: [],
+      originalStart: [],
+      originalEnd: [],
+      isLapseAfterEffectiveDateExpire: [],
+      isCheckin: [],
+      checkInDate: [],
+      isCheckout: [],
+      checkOutDate: [],
+      isPostRent: [],
+      isAllowPayment: [],
+      isOnHold: [],
+      renewalStart: [],
+      renewalRent: [],
+      suggestedTerm: [],
+      suggestedRent: [],
+      suggestedPeriodicity: [],
+      contractType: [],
+      contractSubType: [],
+      isStudentTypeAgreement: [],
+      isChangeOfSharerAgreement: [],
+      tenancyStartDate: [],
+      tenancyEndDate: [],
+      noticeDate: [],
+      noticeProcessedDate: [],
+      isExcludeFromArrears: [],
+      inventoryPreparedBy: []
+    });
+  }
+
+  private getLookupData() {
+    this.lookupdata = this.commonService.getItem(PROPCO.LOOKUP_DATA, true);
+    if (this.lookupdata) {
+      this.setLookupData(this.lookupdata);
+    } else {
+      this.commonService.getLookup().subscribe(data => {
+        this.commonService.setItem(PROPCO.LOOKUP_DATA, data);
+        this.lookupdata = data;
+        this.setLookupData(data);
+      });
+    }
+  }
+
+  private setLookupData(data) {
+    this.frequencyTypes = data.frequencyTypes;
+    this.agreementStatuses = data.agreementStatuses;
+    this.managementTypes = data.managementTypes;
+    this.contractTypes = data.contractTypes;
+    this.agreementPeriodicities = data.agreementPeriodicities;
+    this.inventoryPreparedBy = data.inventoryPreparedBy;
+  }
+
+  private getAgreementDetails() {
+    return new Promise((resolve, _reject) => {
+      this.agentService.getAgreementDetails(this.selectedTenant?.agreementId).subscribe(
+        (res) => {
+          resolve(res ? res : {});
+        },
+        (error) => {
+          resolve(false);
+        }
+      );
+    });
+  }
+
+  private patchAgreementDetails() {
+    this.agreementDetailsForm.patchValue(this.agreementDetails)
+    this.agreementDetailsForm.patchValue({
+      contractType: this.agreementDetails.contractType.toString(),
+      totalOccupants: (this.agreementDetails?.totalOccupants ? this.agreementDetails?.totalOccupants : (this.agreementDetails?.noOfOccupiers + this.agreementDetails?.noOfChildren + this.agreementDetails?.noOfPermittedOccupier)),
+      tenantName: this.agreementDetails.agreementTenantDetail[0].tenantId,
+      rent: this.agreementDetails.agreementTenantDetail[0].rent,
+      nextClaimDate: this.agreementDetails.agreementTenantDetail[0].nextClaimDate,
+      lastRentRequestDate: this.agreementDetails.agreementTenantDetail[0].lastRentRequestDate,
+      renewalRent: this.agreementDetails.agreementTenantDetail[0].renewalRent,
+      room: this.agreementDetails.agreementTenantDetail[0].room
+    });
+  }
+
+  getStatusColorClassName(status) {
+    let className = '';
+    switch (status) {
+      case 3:
+      case 4:
+        className = 'tenancy-status-expired';
+        break;
+      case 2:
+      case 6:
+        className = 'tenancy-status-confirmed';
+        break;
+      case 1:
+      case 5:
+        className = 'tenancy-status-proposed';
+        break;
+      default:
+        className = 'tenancy-status-other';
+        break;
+    }
+    return className;
+  }
+}
---
+++ src/app/agent/workspace/property/admin/tenancy/agreement-history/agreement-history.component.ts
@@ -0,0 +1,68 @@
+import { Component, Input, OnInit, QueryList, SimpleChanges, ViewChildren } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
+import { Subject } from 'rxjs';
+import { AgentService } from 'src/app/agent/agent.service';
+import { DATE_FORMAT } from 'src/app/shared/constants';
+
+@Component({
+  selector: 'app-agreement-history',
+  templateUrl: './agreement-history.component.html',
+  styleUrls: ['./agreement-history.component.scss'],
+})
+export class AgreementHistoryComponent implements OnInit {
+  DATE_FORMAT = DATE_FORMAT;
+  logHistoryList: any;
+  historyDtOption: DataTables.Settings;
+  historyDtTrigger: Subject<any> = new Subject();
+  @ViewChildren(DataTableDirective) dtElements: QueryList<DataTableDirective>;
+  @Input() selectedTenant;
+  @Input() agreementStatusesLookup;
+
+  constructor(private agentService: AgentService) { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.selectedTenant && changes.selectedTenant.currentValue) {
+      this.selectedTenant = this.selectedTenant;
+      this.getHistory();
+    }
+  }
+
+  ngOnInit() {
+    this.historyDtOption = this.buildDtOptions();
+    setTimeout(() => {
+      this.historyDtTrigger.next();
+    }, 100);
+  }
+
+  private getHistory() {
+    this.getAgreementHistory(this.selectedTenant?.agreementId);
+  }
+
+  private buildDtOptions(): DataTables.Settings {
+    return {
+      paging: true,
+      searching: false,
+      ordering: false,
+      responsive: true,
+      lengthMenu: [5, 10, 15],
+      pageLength: 5,
+    };
+  }
+
+  private getAgreementHistory(agreementId: string) {
+    this.agentService.getAgreementHistory(agreementId).subscribe(res => {
+      this.logHistoryList = res && res.data ? res.data : [];
+      this.rerenderHistoryTable();
+    });
+  }
+
+  private rerenderHistoryTable(): void {
+    if (this.dtElements && this.dtElements.last.dtInstance) {
+      this.dtElements.last.dtInstance.then((dtInstance: DataTables.Api) => {
+        dtInstance.destroy();
+        this.historyDtTrigger.next();
+      });
+    }
+  }
+
+}
