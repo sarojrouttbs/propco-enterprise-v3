8a6caaf1852997ddb4ae1884a6f10750dbce369f
gaurid-iprogrammer <gaurid@iprogrammer.com>
2022-08-24 13:25:58 +0530

    PRCV-8168

--- src/app/agent/agent.service.ts
+++ src/app/agent/agent.service.ts
@@ -187,12 +187,6 @@ export class AgentService {
     return this.httpClient.get(environment.API_BASE_URL + `properties/${propertyId}/licences`, { params });
   }
 
-  getViewings(propertyId: string, params: any): Observable<any> {
-    console.log("in service");
-    
-    return this.httpClient.get(environment.API_BASE_URL + `properties/${propertyId}/viewings`, { params });
-  }
-
   getClauses(params: any): Observable<any> {
     return this.httpClient.get(environment.API_BASE_URL + `clauses/node`, { params });
   }
