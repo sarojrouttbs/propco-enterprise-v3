77a2e66d253a60d88113c7b481b948b1a270a87d
gaurid-iprogrammer <gaurid@iprogrammer.com>
2022-08-26 13:50:42 +0530

    PRCV-7684

---
+++ src/app/agent/workspace/property/viewings/grid-view/grid-view.component.ts
@@ -0,0 +1,136 @@
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { AgentService } from 'src/app/agent/agent.service';
+import { AGENT_WORKSPACE_CONFIGS, DATE_FORMAT, DEFAULTS, DEFAULT_MESSAGES } from 'src/app/shared/constants';
+import { CommonService } from 'src/app/shared/services/common.service';
+
+@Component({
+  selector: 'app-grid-view',
+  templateUrl: './grid-view.component.html',
+  styleUrls: ['./grid-view.component.scss'],
+})
+export class GridViewComponent implements OnInit, OnChanges {
+
+  @Input() viewingForm;
+  @Input() viewingsParams;
+  viewingList: any;
+  DATE_FORMAT = DATE_FORMAT;
+  pageEvent: PageEvent;
+  pageIndex = 0;
+  public pageSize = 10;
+  selectedItem: any;
+  notAvailable = DEFAULTS.NOT_AVAILABLE
+  localStorageItems: any;
+  selectedEntityDetails: any;
+  DEFAULT_MESSAGES = DEFAULT_MESSAGES;
+  
+  constructor(
+    private el: ElementRef<HTMLElement>,
+    private agentService: AgentService,
+    private commonService: CommonService
+  ) { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.viewingsParams && changes.viewingsParams.currentValue) {
+      if (this.viewingForm.value.fromDate && this.viewingForm.value.toDate) {
+        this.getViewings();
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.initApi();
+  }
+
+  async initApi() {
+    this.localStorageItems = await this.fetchItems();
+    this.selectedEntityDetails = await this.getActiveTabEntityInfo();
+    this.getViewings();
+  }
+
+  private fetchItems() {
+    return (
+      this.commonService.getItem(
+        AGENT_WORKSPACE_CONFIGS.localStorageName,
+        true
+      ) || []
+    );
+  }
+
+  private getActiveTabEntityInfo() {
+    const item = this.localStorageItems.filter((x) => x.isSelected);
+    if (item) {
+      return item[0];
+    }
+  }
+
+  private getViewings() {
+    this.agentService.getViewings(this.selectedEntityDetails.entityId, this.viewingsParams).subscribe(res => {
+      this.viewingList = res && res.data ? res.data : [];
+      this.customizePaginator();
+    })
+  }
+
+
+  private customizePaginator(): void {
+    setTimeout(() => {
+      const lastBtn = this.el.nativeElement.querySelector(
+        '.mat-paginator-navigation-last'
+      );
+      if (lastBtn) {
+        lastBtn.innerHTML = 'Last';
+      }
+      const firstBtn = this.el.nativeElement.querySelector(
+        '.mat-paginator-navigation-first'
+      );
+      if (firstBtn) {
+        firstBtn.innerHTML = 'First';
+      }
+
+      const perPage = this.el.nativeElement.querySelector(
+        '.mat-paginator-page-size-label'
+      );
+      if (perPage) {
+        perPage.innerHTML = 'Per page';
+      }
+    }, 100);
+  }
+
+  public handlePage(e: PageEvent) {
+    this.pageIndex = e.pageIndex;
+    this.pageSize = e.pageSize;
+    this.getViewings();
+    return e;
+  }
+
+  showMenu(event: any, id: any, data: any, className: any) {
+    this.selectedItem = data;
+    const baseContainer = $(event.target).parents('.' + className);
+    const divOverlay = $('#' + id);
+    const baseContainerWidth = baseContainer.outerWidth(true) - 130;
+    const baseContainerHeight = baseContainer.outerHeight(true) - 10;
+    const baseContainerPosition = baseContainer.position();
+    const baseContainerTop = baseContainerPosition.top + 5;
+    const divOverlayLeft = baseContainerPosition.left + 125;
+
+    divOverlay.css({
+      position: 'absolute',
+      top: baseContainerTop,
+      right: '10px',
+      width: baseContainerWidth,
+      height: baseContainerHeight,
+      left: divOverlayLeft,
+      padding: '10px',
+      paddingRight: '15px',
+      borderTopRightRadius: '4px',
+      borderBottomRightRadius: '4px'
+    });
+
+    divOverlay.delay(200).slideDown('fast');
+    event.stopPropagation();
+  }
+
+  hideMenu(event: any, id: any) {
+    this.commonService.hideMenu(event, id);
+  }
+}
---
+++ src/app/agent/workspace/property/viewings/viewings.component.ts
@@ -0,0 +1,102 @@
+import { HttpParams } from '@angular/common/http';
+import { Component, Input, OnInit, QueryList, SimpleChanges, ViewChild, ViewChildren } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { DataTableDirective } from 'angular-datatables';
+import { DATE_FORMAT, DATE_RANGE_CONFIG, DATE_RANGE_CONFIG_LIST } from 'src/app/shared/constants';
+import { CommonService } from 'src/app/shared/services/common.service';
+
+@Component({
+  selector: 'app-viewings',
+  templateUrl: './viewings.component.html',
+  styleUrls: ['./viewings.component.scss'],
+})
+
+export class ViewingsComponent implements OnInit {
+
+  @Input() viewingInstructions;
+  viewType = 'grid';
+  viewings: any;
+  dtOptions: any = {};
+  @ViewChildren(DataTableDirective) dtElements: QueryList<DataTableDirective>;
+  viewingsParams: any = new HttpParams();
+  viewingForm: FormGroup;
+  DATE_RANGE_CONFIG_LIST = DATE_RANGE_CONFIG_LIST;
+  DATE_FORMAT = DATE_FORMAT;
+  currentDate = new Date();
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private commonService: CommonService,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.viewingInstructions && changes.viewingInstructions.currentValue) {
+      this.viewingForm.get('viewingInstructions').setValue(this.viewingInstructions);
+    }
+  }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  switchView(type) {
+    this.viewType = type;
+  }
+
+  initForm() {
+    this.viewingForm = this.fb.group({
+      quickFilterType: [''],
+      fromDate: [''],
+      toDate: [''],
+      viewingInstructions: ['']
+    });
+  }
+
+  onFilterChange() {
+    const selectedRange = this.viewingForm.value.quickFilterType;
+    this.viewingForm.get('toDate').setValue(this.commonService.getFormatedDate(this.currentDate, this.DATE_FORMAT.YEAR_DATE));
+    switch (selectedRange) {
+      case DATE_RANGE_CONFIG.WEEK_TO_DATE:
+        this.getFirstDayOfWeek(new Date())
+        const firstDayOfWeek = this.getFirstDayOfWeek(new Date());
+        this.viewingForm.get('fromDate').setValue(this.commonService.getFormatedDate(firstDayOfWeek, this.DATE_FORMAT.YEAR_DATE));
+        break;
+      case DATE_RANGE_CONFIG.MONTH_TO_DATE:
+        const firstDayOfMonth = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), 1);
+        this.viewingForm.get('fromDate').setValue(this.commonService.getFormatedDate(firstDayOfMonth, this.DATE_FORMAT.YEAR_DATE));
+        break;
+      case DATE_RANGE_CONFIG.YEAR_TO_DATE:
+        const firstDayOfYear = new Date(this.currentDate.getFullYear(), 0);
+        this.viewingForm.get('fromDate').setValue(this.commonService.getFormatedDate(firstDayOfYear, this.DATE_FORMAT.YEAR_DATE));
+        break;
+      case DATE_RANGE_CONFIG.LAST_MONTH:
+        const firstDayOfLastMonth = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() - 1, 1);
+        const lastDayOfLastMonth = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() - 0, 0);
+        this.viewingForm.get('fromDate').setValue(this.commonService.getFormatedDate(firstDayOfLastMonth, this.DATE_FORMAT.YEAR_DATE));
+        this.viewingForm.get('toDate').setValue(this.commonService.getFormatedDate(lastDayOfLastMonth, this.DATE_FORMAT.YEAR_DATE));
+        break;
+    }
+  }
+
+  private getFirstDayOfWeek(currentDate: Date) {
+    const date = new Date(currentDate);
+    const day = (date.getDay() + 1);
+    const diff = date.getDate() - day;
+    return new Date(date.setDate(diff));
+  }
+
+  applyFilter() {
+    this.viewingsParams = this.viewingsParams.set('dateRange.from', this.viewingForm.value.fromDate ? this.commonService.getFormatedDate(this.viewingForm.value.fromDate, this.DATE_FORMAT.YEAR_DATE) : '');
+    this.viewingsParams = this.viewingsParams.set('dateRange.to', this.viewingForm.value.toDate ? this.commonService.getFormatedDate(this.viewingForm.value.toDate, this.DATE_FORMAT.YEAR_DATE) : '');
+  }
+
+  resetFilter() {
+    this.viewingForm.reset();
+  }
+
+
+
+
+}
