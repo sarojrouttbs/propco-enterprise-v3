e32fd8ce95ec42e20b7253e63faef3231357dce9
saroj.rout <saroj.rout@techblue.co.uk>
2023-04-24 13:59:20 +0530

    reverted debugging mode : solr

--- src/lib/java-ng.js
+++ src/lib/java-ng.js
@@ -1,14 +1,6 @@
 function openScreen(key, value, existing = false) {
-    console.log({
-        request: !value ? `["${key}"]` : (`["${key}" , "${value}", "${existing}"]`),
-        persistent: false,
-        onSuccess: function (response) {
-            print(response);
-        },
-        onFailure: function (error_code, error_message) {}
-    })
     window.propCoFunction({
-        request: !value ? `["${key}"]` : (`["${key}" , "5181", "${existing}"]`),
+        request: !value ? `["${key}"]` : (`["${key}" , "${value}", "${existing}"]`),
         persistent: false,
         onSuccess: function (response) {
             print(response);
